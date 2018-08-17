d3a27c7e1a07a6670c2fb3c64792e5d72a9f8d91
Josh Wardle <josh.wardle@gmail.com>
2018-08-17 13:16:16 -0700

    Add recipient parsing and ignoring

--- phabulous.js
+++ phabulous.js
@@ -24,9 +24,13 @@ var PHABRICATOR_GMAIL_LABEL = 'FILL_ME_IN';
 // How many conversations should gmail fetch at a time
 var BATCH_SIZE = 10;
 
+// Recipients that you don't want notifications for (e.g. noisy groups)
+var IGNORED = ['FILL_ME_IN']
+
 /* --- Leave this section --- */
 
-var OWNER_REGEX = /To:\s([a-zA-Z0-9]*)/
+var PHAB_TO_REGEX = /To: ([a-zA-Z0-9 \t,#-_]*)/
+var PHAB_CC_REGEX = /Cc: ([a-zA-Z0-9 \t,#-_]*)/
 var SENDER_REGEX = /^"?(\w*)/
 var ACTIONS_REGEX = /([\s\S]*?)[\n\r][\n\r]/
 var TITLE_REGEX = /: (.*)/
@@ -84,16 +88,35 @@ function formatActions(actions) {
   }
 }
 
+function convertRecipientsStringToArray(phabString) {
+  return phabString.trim().split(',').map(function(item){
+    return item.trim();
+  });
+}
+
+function shouldIgnoreAnyRecipient(recipients) {
+  for(var i = 0; i < IGNORED.length; i++) {
+    if(recipients.indexOf(IGNORED[i]) > -1) {
+     return true;
+    }
+  }
+  return false;
+}
+
 function parsePhabricatorEmail(email) {
   var details = {};
   details.body = email.getBody();
   details.from = email.getFrom();
   details.subject = email.getSubject();
-  details.diffOwner = checkRegex(OWNER_REGEX, details.body, 'OWNER')
+  details.phabricatorTo = convertRecipientsStringToArray(checkRegex(PHAB_TO_REGEX, details.body, 'PHAB TO:'));
+  details.phabricatorCC = convertRecipientsStringToArray(checkRegex(PHAB_CC_REGEX, details.body, 'PHAB CC:'));
+  // This whole thing works because of the below assumption that the first person in the To: field is the diff author
+  details.diffOwner = details.phabricatorTo[0];
   details.sender = checkRegex(SENDER_REGEX, details.from, 'SENDER');
   details.diffTitle = checkRegex(TITLE_REGEX, details.subject, 'TITLE');
   details.diffUrl = checkRegex(URL_REGEX, details.body, 'URL');
   details.isBuildMessage = isBuildMessage(details.sender, details.body);
+  details.shouldIgnore = shouldIgnoreAnyRecipient(details.phabricatorTo);
   details.actions = filterActions(checkRegex(ACTIONS_REGEX, details.body, 'ACTIONS'), details.sender);
   details.formattedActions = formatActions(details.actions);
   return details;
@@ -112,13 +135,12 @@ function phabulous() {
     thread.getMessages().forEach(function(email) {
       if (email.isUnread()) {
         var details = parsePhabricatorEmail(email);
-
         if(details.diffOwner === MY_LDAP && !details.isBuildMessage) {
           // If I own this diff, notify me of everything (except builds)
           outgoingEmail.subject = details.sender + ' ' + details.formattedActions + ': ' + details.diffTitle;
           outgoingEmail.body = details.diffUrl;
-        } else if(details.formattedActions.indexOf('requested review') > -1) {
-          // If I don't own this diff, only notify me of requests for review
+        } else if(!details.shouldIgnore && details.formattedActions.indexOf('requested review') > -1) {
+          // If I don't own this diff and recipient is not ignored, notify me of requests for review
           outgoingEmail.subject = details.sender + ' ' + details.formattedActions + ': ' + details.diffTitle;
           outgoingEmail.body = details.diffUrl;
         }
